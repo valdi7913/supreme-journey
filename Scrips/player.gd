4fd8b52dddd43f1f92dc03fd22e458fa1e89a71e
valdi7913 <68957139+valdi7913@users.noreply.github.com>
2024-02-04 23:14:00 +0000

    Added Charge Jump Mechanic

--- Scrips/player.gd
+++ Scrips/player.gd
@@ -1,19 +1,18 @@
 extends RigidBody2D
 
-@export var speed : float = 400 # How fast the player will move (pixels/sec).
-@export var jump_force : float = 3000000
+@export var MAX_JUMP_FORCE : float = 10000
+@export var MAX_JUMP_CHARGE_TIME : float = 4
 var EPSILON : float = 40
 var jump_angle : float = 180
-var angle_diff : float = 30
-var is_airborne : bool = true
-
+@export var angle_diff : float = 30
+var is_airborne : bool = true 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("default")
 	pass
 
-func _process(delta):	
+func _process(delta):
 	_handle_input()
 	_handle_animation()
 
@@ -24,20 +23,19 @@ func deg_to_rad(deg : float):
 	return PI/180 * deg 
 
 func _handle_input():
+	if Input.is_action_just_pressed("jump") and !is_airborne:
+		$JumpTimer.stop()
+		$JumpTimer.start(MAX_JUMP_CHARGE_TIME)
+		print("Time to max jump ", MAX_JUMP_CHARGE_TIME)
+	
+	
 	if Input.is_action_pressed("move_left"):
 		$AnimatedSprite2D.flip_h = true
 	if Input.is_action_pressed("move_right"):
 		$AnimatedSprite2D.flip_h = false
 	
-	if Input.is_action_just_pressed("jump") and !is_airborne:
-		var jump_direction = Vector2(0,-1).normalized() * jump_force
-		if Input.is_action_pressed("move_left"):
-			jump_direction = jump_direction.rotated(deg_to_rad(-angle_diff))
-		if Input.is_action_pressed("move_right"):
-			jump_direction = jump_direction.rotated(deg_to_rad(angle_diff))	
-		print(jump_direction)
-		apply_force(jump_direction,center_of_mass)
-		is_airborne = true
+	if Input.is_action_just_released("jump") and !is_airborne:
+		jump()
 
 
 func _handle_animation():
@@ -45,3 +43,17 @@ func _handle_animation():
 		$AnimatedSprite2D.flip_h = true
 	elif linear_velocity.x > EPSILON:
 		$AnimatedSprite2D.flip_h = false
+
+func jump():
+	print("Time left", $JumpTimer.time_left)
+	var jump_power = (MAX_JUMP_CHARGE_TIME - $JumpTimer.time_left)/ MAX_JUMP_CHARGE_TIME
+	jump_power = clamp(jump_power, 0, 1)
+	print("jump power ", jump_power)
+	var jump_direction = Vector2(0,-1).normalized() * jump_power * 100000
+	if Input.is_action_pressed("move_left"):
+		jump_direction = jump_direction.rotated(deg_to_rad(-angle_diff))
+	if Input.is_action_pressed("move_right"):
+		jump_direction = jump_direction.rotated(deg_to_rad(angle_diff))	
+	print(jump_direction)
+	apply_force(jump_direction,center_of_mass)
+	#is_airborne = true
