b7c1c898e74fbc1d2d4d812732b25879f1cc5371
valdi7913 <68957139+valdi7913@users.noreply.github.com>
2024-02-04 21:41:30 +0000

    Fixed Bills animation and added bricks

--- Scrips/player.gd
+++ Scrips/player.gd
@@ -1,13 +1,16 @@
 extends RigidBody2D
 
 @export var speed : float = 400 # How fast the player will move (pixels/sec).
-@export var jump_force : float = 30000
+@export var jump_force : float = 3000000
+var EPSILON : float = 40
 var jump_angle : float = 180
 var angle_diff : float = 30
 var is_airborne : bool = true
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimatedSprite2D.play("default")
 	pass
 
 func _process(delta):	
@@ -21,19 +24,24 @@ func deg_to_rad(deg : float):
 	return PI/180 * deg 
 
 func _handle_input():
+	if Input.is_action_pressed("move_left"):
+		$AnimatedSprite2D.flip_h = true
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite2D.flip_h = false
+	
 	if Input.is_action_just_pressed("jump") and !is_airborne:
 		var jump_direction = Vector2(0,-1).normalized() * jump_force
 		if Input.is_action_pressed("move_left"):
-			jump_direction = jump_direction.rotated(deg_to_rad(angle_diff))
+			jump_direction = jump_direction.rotated(deg_to_rad(-angle_diff))
 		if Input.is_action_pressed("move_right"):
-			jump_direction = jump_direction.rotated(deg_to_rad(-angle_diff))	
+			jump_direction = jump_direction.rotated(deg_to_rad(angle_diff))	
 		print(jump_direction)
 		apply_force(jump_direction,center_of_mass)
 		is_airborne = true
 
 
 func _handle_animation():
-	if linear_velocity.x < 0:
+	if linear_velocity.x < -EPSILON:
 		$AnimatedSprite2D.flip_h = true
-	elif linear_velocity.x > 0:
+	elif linear_velocity.x > EPSILON:
 		$AnimatedSprite2D.flip_h = false
