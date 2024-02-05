ee7f80558104a96bfd1e304e585205ad21ada7dd
valdi7913 <68957139+valdi7913@users.noreply.github.com>
2024-02-05 00:13:21 +0000

    Added charge meter

--- Scrips/player.gd
+++ Scrips/player.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 @export var MAX_JUMP_FORCE : float = 10000
-@export var MAX_JUMP_CHARGE_TIME : float = 4
+@export var MAX_JUMP_CHARGE_TIME : float = 2
 var EPSILON : float = 40
 var jump_angle : float = 180
 @export var angle_diff : float = 30
@@ -23,32 +23,30 @@ func deg_to_rad(deg : float):
 	return PI/180 * deg 
 
 func _handle_input():
+	if Input.is_action_just_pressed("reset"):
+		position = Vector2(400,400)
+		rotation = 0
 	if Input.is_action_just_pressed("jump") and !is_airborne:
+		$JumpChargeBar/AnimatedSprite2D.play()
 		$JumpTimer.stop()
 		$JumpTimer.start(MAX_JUMP_CHARGE_TIME)
-		print("Time to max jump ", MAX_JUMP_CHARGE_TIME)
-	
-	
-	if Input.is_action_pressed("move_left"):
-		$AnimatedSprite2D.flip_h = true
-	if Input.is_action_pressed("move_right"):
-		$AnimatedSprite2D.flip_h = false
-	
+		print("Time to max jump ", MAX_JUMP_CHARGE_TIME)	
 	if Input.is_action_just_released("jump") and !is_airborne:
 		jump()
 
 
-func _handle_animation():
-	if linear_velocity.x < -EPSILON:
-		$AnimatedSprite2D.flip_h = true
-	elif linear_velocity.x > EPSILON:
-		$AnimatedSprite2D.flip_h = false
+func _handle_animation():	
+	if (linear_velocity.x < -EPSILON) or Input.is_action_pressed("move_left") :
+		$AnimatedSprite2D.play("default_left")
+	elif linear_velocity.x > EPSILON or Input.is_action_pressed("move_right"):
+		$AnimatedSprite2D.play("default_right")
 
 func jump():
 	print("Time left", $JumpTimer.time_left)
+	$JumpChargeBar/AnimatedSprite2D.stop()
 	var jump_power = (MAX_JUMP_CHARGE_TIME - $JumpTimer.time_left)/ MAX_JUMP_CHARGE_TIME
 	jump_power = clamp(jump_power, 0, 1)
-	print("jump power ", jump_power)
+	print("jump power ", jump_power * 100, " %")
 	var jump_direction = Vector2(0,-1).normalized() * jump_power * 100000
 	if Input.is_action_pressed("move_left"):
 		jump_direction = jump_direction.rotated(deg_to_rad(-angle_diff))
